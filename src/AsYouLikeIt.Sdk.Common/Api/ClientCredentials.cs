170d2c1e61325920b7699f82724074e4fe4855da
John Fager <johndfager@gmail.com>
2024-12-22 21:00:14 -0700

    Update and add functionality

---
+++ src/AsYouLikeIt.Sdk.Common/Api/ClientCredentials.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AsYouLikeIt.Sdk.Common.Api
+{
+    public class ClientCredentials
+    {
+        [JsonPropertyName("client_id")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyOrder(1)]
+        public string ClientId { get; set; }
+
+        [JsonPropertyName("client_secret")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyOrder(2)]
+        public string ClientSecret { get; set; }
+    }
+}
