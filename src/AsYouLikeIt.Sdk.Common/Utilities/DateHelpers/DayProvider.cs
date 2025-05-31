f72543a125baf0401c1a5a2eb6b70055b80b8cc3
John Fager <johndfager@gmail.com>
2025-05-31 09:48:17 -0600

    Cleanup Day

--- src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/DayProvider.cs
+++ src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/DayProvider.cs
@@ -12,59 +12,8 @@ namespace AsYouLikeIt.Sdk.Common.Utilities.DateHelpers
     {
         public override DateTime GetStartOfCurrent(DateTime date) => date.Date;
 
-        public override DateTime GetStartOfNext(DateTime date) => GetStartOfCurrent(date).AddDays(1);
+        public override DateTime Increment(DateTime date, int units) => date.AddDays(units);
 
-        /// <summary>
-        /// Gets the end of the current day.
-        /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
-        public override DateTime GetEndOfCurrent(DateTime date) => date.Date.AddDays(1).AddTicks(-1);
-
-        public override DateTime GetEndOfPrevious(DateTime date) => GetEndOfCurrent(date).AddDays(-1);
-
-        public override HashSet<DateTime> GetStartingDates(DateTime startDate, DateTime endDate)
-        {
-            var beginningDate = startDate.Date;
-            var lastDate = endDate.Date;
-
-            // -----------------------------------------------------------------------------
-            //  use only the beginningDate and lastDate to create a set of dates from here
-            // -----------------------------------------------------------------------------
-
-            var dates = new HashSet<DateTime>();
-            DateTime currentDate = beginningDate.Date;
-            while (currentDate <= lastDate)
-            {
-                // get the last day of the month of the currentDate
-                dates.Add(currentDate);
-                currentDate = currentDate.Date.AddDays(1);
-            }
-            return dates;
-        }
-
-        public override HashSet<DateTime> GetEndingDates(DateTime startDate, DateTime endDate, bool completedTermsOnly)
-        {
-            var beginningDate = GetEndOfCurrent(startDate);
-
-            // if completedTermsOnly is true, we need to find the last completed week. otherwise, we can use the endDate directly.
-            var lastDate = completedTermsOnly ?
-                GetEndOfPrevious(endDate) :
-                GetEndOfCurrent(endDate);
-
-            // -----------------------------------------------------------------------------
-            //  use only the beginningDate and lastDate to create a set of dates from here
-            // -----------------------------------------------------------------------------
-
-            var dates = new HashSet<DateTime>();
-            DateTime currentDate = beginningDate;
-            while (currentDate <= lastDate)
-            {
-                // get the last day of the month of the currentDate
-                dates.Add(currentDate);
-                currentDate = currentDate.Date.AddDays(1);
-            }
-            return dates;
-        }
+        protected override DateTime SetToEndTerm(DateTime date) => date.Date.AddDays(1).AddTicks(-1);
     }
 }
--- src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/MonthProvider.cs
+++ src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/MonthProvider.cs
@@ -2,14 +2,18 @@
 
 namespace AsYouLikeIt.Sdk.Common.Utilities.DateHelpers
 {
+    public static class Month
+    {
+        public static MonthProvider Provider { get; } = new MonthProvider();
+    }
+
     public class MonthProvider : DatePeriodProviderBase
     {
         public override DateTime GetStartOfCurrent(DateTime date) => new DateTime(date.Year, date.Month, 1);
 
         public override DateTime Increment(DateTime date, int units) => GetStartOfCurrent(date).AddMonths(units);
 
-        protected override DateTime SetToEndTerm(DateTime date)
-            => new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+        protected override DateTime SetToEndTerm(DateTime date) => new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
 
     }
 }
