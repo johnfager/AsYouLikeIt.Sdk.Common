43819039fb73043c9a08e0df2027c17d4c718874
John Fager <johndfager@gmail.com>
2025-05-31 08:54:57 -0600

    Clean up base and plug in DayProvider

---
+++ src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/DatePeriodProviderBase.cs
@@ -0,0 +1,106 @@
+﻿using AsYouLikeIt.Sdk.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsYouLikeIt.Sdk.Common.Utilities.DateHelpers
+{
+    /// <summary>
+    /// Base provider to keep all term providers consistent for functionality and testing. 
+    /// Shares common logic and avoids code duplication.
+    /// </summary>
+    public abstract class DateHelperProviderBase
+    {
+        public abstract DateTime GetStartOfCurrent(DateTime date);
+
+        public abstract DateTime GetStartOfNext(DateTime date);
+
+        public abstract DateTime GetEndOfCurrent(DateTime date);
+
+        public abstract DateTime GetEndOfPrevious(DateTime date);
+
+        public abstract HashSet<DateTime> GetStartingDates(DateTime startDate, DateTime endDate);
+
+        public abstract HashSet<DateTime> GetEndingDates(DateTime startDate, DateTime endDate, bool completedTermsOnly);
+
+        public List<IDateRange> GetTermRanges(DateTime startDate, DateTime endDate, bool completedTermsOnly, bool includeCompleteInitialTermsOnly)
+        {
+            var ranges = new List<IDateRange>();
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date cannot be after end date.", nameof(startDate));
+            }
+
+            var startOfWeekDates = GetStartingDates(startDate, endDate).ToArray();
+            var endOfWeekDates = GetEndingDates(startDate, endDate, completedTermsOnly).ToArray();
+
+            var validatedRanges = GetValidatedDateRanges(startOfWeekDates, endOfWeekDates, completedTermsOnly);
+            return validatedRanges;
+        }
+
+        #region helpers
+
+        private void ValidateDateArrayCounts(DateTime[] start, DateTime[] end, bool completedTermsOnly)
+        {
+            if (start == null || end == null)
+            {
+                throw new ArgumentNullException("Start and end date arrays cannot be null.");
+            }
+
+            if (!completedTermsOnly && start.Length != end.Length)
+            {
+                throw new InvalidOperationException("The number of start and end values must match when completedTermsOnly is false.");
+            }
+            // verifify that the range is equal or end + 1 equal to start
+            if (completedTermsOnly &&
+                !(start.Length == end.Length || start.Length == end.Length + 1))
+            {
+                throw new InvalidOperationException("The number of start and end values must match or end can have one less value if completedTermsOnly is true.");
+            }
+        }
+
+        protected List<IDateRange> GetValidatedDateRanges(DateTime[] start, DateTime[] end, bool completedTermsOnly)
+        {
+            // ensure not out of bounds
+            ValidateDateArrayCounts(start, end, completedTermsOnly);
+
+            var ranges = new List<IDateRange>();
+
+            if (end.Length == 0)
+            {
+                // if there are no end dates, we cannot create any ranges
+                return ranges;
+            }
+
+            var currentStart = start[0];
+            var currentEnd = end[0];
+
+            // We don't always take the last start date to make a range, so use the ends
+            for (int i = 0; i < end.Length; i++)
+            {
+                var startOfTerm = start[i];
+                if (startOfTerm < currentStart)
+                {
+                    throw new ArgumentException($"Start dates missorted. Start date {startOfTerm} cannot be earlier than the current start date {currentStart}.");
+                }
+                var endOfTerm = end[i];
+                if (endOfTerm < currentEnd)
+                {
+                    throw new ArgumentException($"End dates missorted. End date {endOfTerm} cannot be earlier than the current end date {currentEnd}.");
+                }
+
+                // date range already validates the start and end dates
+                ranges.Add(new DateRange(startOfTerm, endOfTerm));
+
+                // set the outer DateTime holder to ensure the next range is valid compared to the last one
+                currentStart = startOfTerm;
+                currentEnd = endOfTerm;
+            }
+
+            return ranges;
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/AsYouLikeIt.Sdk.Common/Utilities/DateHelpers/DayProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsYouLikeIt.Sdk.Common.Utilities.DateHelpers
+{
+    public class DayProvider : DatePeriodProviderBase
+    {
+        public override DateTime GetStartOfCurrent(DateTime date) => date.Date;
+
+        public override DateTime GetStartOfNext(DateTime date) => GetStartOfCurrent(date).AddDays(1);
+
+        /// <summary>
+        /// Gets the end of the current day.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public override DateTime GetEndOfCurrent(DateTime date) => date.Date.AddDays(1).AddTicks(-1);
+
+        public override DateTime GetEndOfPrevious(DateTime date) => GetEndOfCurrent(date).AddDays(-1);
+
+        public override HashSet<DateTime> GetStartingDates(DateTime startDate, DateTime endDate)
+        {
+            var beginningDate = startDate.Date;
+            var lastDate = endDate.Date;
+
+            // -----------------------------------------------------------------------------
+            //  use only the beginningDate and lastDate to create a set of dates from here
+            // -----------------------------------------------------------------------------
+
+            var dates = new HashSet<DateTime>();
+            DateTime currentDate = beginningDate.Date;
+            while (currentDate <= lastDate)
+            {
+                // get the last day of the month of the currentDate
+                dates.Add(currentDate);
+                currentDate = currentDate.Date.AddDays(1);
+            }
+            return dates;
+        }
+
+        public override HashSet<DateTime> GetEndingDates(DateTime startDate, DateTime endDate, bool completedTermsOnly)
+        {
+            var beginningDate = GetEndOfCurrent(startDate);
+
+            // if completedTermsOnly is true, we need to find the last completed week. otherwise, we can use the endDate directly.
+            var lastDate = completedTermsOnly ?
+                GetEndOfPrevious(endDate) :
+                GetEndOfCurrent(endDate);
+
+            // -----------------------------------------------------------------------------
+            //  use only the beginningDate and lastDate to create a set of dates from here
+            // -----------------------------------------------------------------------------
+
+            var dates = new HashSet<DateTime>();
+            DateTime currentDate = beginningDate;
+            while (currentDate <= lastDate)
+            {
+                // get the last day of the month of the currentDate
+                dates.Add(currentDate);
+                currentDate = currentDate.Date.AddDays(1);
+            }
+            return dates;
+        }
+    }
+}
